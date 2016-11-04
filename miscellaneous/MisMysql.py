ccf984ccecd8ae1b1ad4ecc4aa54729b0cf11c4a
fumanne <fu-l@klab.com>
2016-11-04 22:06:44 +0800

    todo mysql

--- miscellaneous/MisMysql.py
+++ miscellaneous/MisMysql.py
@@ -11,28 +11,57 @@ class BaseDB(object):
         self.port = port
         self.charset = charset
         self.connect_timeout = connect_timeout
-        try:
-            self.conn = mc.connect(host=self.host, user=self.user, password=self.password, port=self.port,
+
+        self.conn = None
+        self.cur = None
+        self.is_connected = False
+        self.is_cur_connected = False
+
+
+
+    def __repr__(self):
+        return "{0}.connection".format(self.__class__.__name__)
+
+
+    def connect(self):
+        if not self.is_connected:
+            try:
+                _conn = mc.connect(host=self.host, user=self.user, password=self.password, port=self.port,
                                    charset=self.charset, connect_timeout=self.connect_timeout)
-        except mc.Error as e:
-            raise DBError(e)
-        else:
-            self.cursor = self.conn.cursor()
-            #   buffered=None, raw=None, prepared=None, cursor_class=None,
-            #   dictionary=None, named_tuple=None
-            #   you can use any method you like, just like: self.conn.cursor(dictionary=True) or
-            #   self.conn.cursor(buffered=True, dictionary=True) and so on .....
+            except mc.Error as e:
+                raise DBError(e)
+            else:
+                self.conn = _conn
+                self.is_connected = True
+                return self.conn
+
+    def cursor(self, **kwargs):
+        all_keys = ('buffered', 'raw', 'prepared', 'dictionary', 'named_tuple')
+        for k in kwargs.keys():
+            if k not in all_keys:
+                raise KeyError("{} is not support".format(k))
+
+        try:
+            self.cur = self.conn.curosr(**kwargs)
+        except:
+            raise DBError()
+        self.is_cur_connected = True
+        return self.cur
 
     def commit_or_rollback(self, sqlcmd):
+        if not self.is_cur_connected:
+            raise DBError('curosr is not connected')
+
         try:
-            self.cursor.execute(sqlcmd)
+            self.cur.execute(sqlcmd)
         except mc.Error as e:
             self.conn.rollback()
             raise DBError(e)
         else:
             self.conn.commit()
 
-    # TODO: add more function to check the mysql status or some set functions
+
+    # TODO: Meaningless  will be deleted in future
 
     def __del__(self):
         self.conn.close()
